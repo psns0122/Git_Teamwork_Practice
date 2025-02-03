181dbbb553ae3f5e3f3faf57a58c52b024190f4f
psns <psns0122@naver.com>
2025-02-03 09:30:04 +0900

    UPDATE NO5

--- Gunhee/250124/homework/NO5/Account.java
+++ Gunhee/250124/homework/NO5/Account.java
@@ -1,4 +1,51 @@
 package day13.homework.NO5;
 
-public class Account {
+public abstract class Account {
+    // 멤버 변수
+    private String accId;
+    private long balance;
+    private String ownerName;
+
+    // 생성자
+    public Account(){
+
+    }
+    public Account(String accId,long balance,String ownerName){
+        this.accId = accId;
+        this.balance = balance;
+        this.ownerName = ownerName;
+    }
+
+    // 추상 메소드
+    public abstract void calcRate();
+
+    // 멤버 메소드
+    public void deposit(long money){
+        this.balance = this.balance + money;
+
+    }
+    public void withdraw(long money){
+        this.balance = this.balance - money;
+    }
+
+    // getter, setter 메소드
+    public long getBalance(){
+        return this.balance;
+    }
+    public String getAccId() {
+        return accId;
+    }
+    public void setAccId(String accId) {
+        this.accId = accId;
+    }
+    public String getOwnerName() {
+        return ownerName;
+    }
+    public void setOwnerName(String ownerName) {
+        this.ownerName = ownerName;
+    }
+    public void setBalance(long balance) {
+        this.balance = balance;
+    }
+
 }
--- Gunhee/250124/homework/NO5/CheckingAccount.java
+++ Gunhee/250124/homework/NO5/CheckingAccount.java
@@ -1,4 +1,43 @@
 package day13.homework.NO5;
 
-public class CheckingAccount {
+public class CheckingAccount extends Account {
+    // 멤버 변수
+    private String cardNo;
+
+    // getter, setter 메소드
+    public String getCardNo() {
+        return cardNo;
+    }
+    public void setCardNo(String cardNo) {
+        this.cardNo = cardNo;
+    }
+
+    // 생성자
+    CheckingAccount(String accId, String ownerName, long balance, String cardNo) {
+        this.cardNo = cardNo;
+        this.setAccId(accId);
+        this.setBalance(balance);
+        this.setOwnerName(ownerName);
+    }
+
+    // 멤버 메소드
+    void pay(String cardNo, long amount) {
+        if (this.cardNo == cardNo) {
+            if (amount < this.getBalance()) {
+                this.setBalance(this.getBalance() - amount);
+            }
+            else {
+                System.out.println("지불이 불가능합니다.");
+            }
+        }
+        else {
+            System.out.println("지불이 불가능합니다.");
+        }
+    }
+
+    // 추상 메소드 오버라이딩
+    @Override
+    public void calcRate() {
+        // 아직 기능 정의가 되지 않아서 구현하지 않음
+    }
 }
