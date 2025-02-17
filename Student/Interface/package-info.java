e1af4fdde8eaf72110d0fd94a6df3adc8f1a219b
psns0122 <psns0122@naver.com>
2025-02-17 11:50:53 +0900

    ADD package javadoc

---
+++ Student/Interface/package-info.java
@@ -0,0 +1,17 @@
+/**
+ * 이 패키지는 학교 관리 시스템에서 다양한 인터페이스와
+ * InterfaceIO 유틸리티 클래스를 포함한 기능성 패키지입니다.
+ * <p>
+ * 주요 구성 요소: <br>
+ * - {@link Interface.Addable}: 객체 추가 기능 제공 인터페이스 <br>
+ * - {@link Interface.Searchable}: 객체 탐색 기능 제공 인터페이스 <br>
+ * - {@link Interface.Printable}: 객체 출력 기능 제공 인터페이스 <br>
+ * - {@link Interface.Sortable}: 객체 정렬 기능 제공 인터페이스 <br>
+ * - {@link Interface.Calculable}: 계산 관련 기능 제공 인터페이스 <br>
+ * - {@link Interface.InterfaceIO}: 다양한 인터페이스를 활용한 공통 유틸리티 클래스
+ * <p>
+ * 이 패키지는 다양한 인터페이스들을 통해 기능을 모듈화하며,
+ * InterfaceIO를 통해 공통 작업을 수행할 수 있도록 지원합니다.
+ */
+
+package Interface;
---
+++ Student/ObjectClass/package-info.java
@@ -0,0 +1,12 @@
+/**
+ * 이 패키지는 학교 관리 시스템의 핵심 클래스(Student, Employee)를 포함한
+ * ObjectClass 패키지입니다.
+ * <p>
+ * 주요 구성 요소: <br>
+ * - {@link ObjectClass.Student}: 학생 정보를 관리하는 클래스 <br>
+ * - {@link ObjectClass.Employee}: 직원 정보를 관리하는 클래스
+ * <p>
+ * 이 패키지는 학교 구성원(학생 및 직원)의 정보를 포함하며,
+ * 각 클래스는 이름, 번호 등 주요 정보를 관리하는 기능을 제공합니다.
+ */
+package ObjectClass;
