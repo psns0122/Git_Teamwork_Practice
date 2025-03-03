093d828193dacfce875ce65d28f2c947aca861fc
seonmin12 <seonmink12@naver.com>
2025-03-03 19:31:27 +0900

    BoardPractice

---
+++ BoardTeamPractice/controller/BoardMain.java
@@ -0,0 +1,8 @@
+package BoardTeamPractice.controller;
+
+public class BoardMain {
+    public static void main(String[] args) {
+        BoardMenu boardMenu = new BoardMenu();
+        boardMenu.list();
+    }
+}
